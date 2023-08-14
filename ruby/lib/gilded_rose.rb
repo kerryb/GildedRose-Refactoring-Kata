634edde579ad1224f7fd60c28c9a424b1fbe6c11
Kerry Buckley <kerryjbuckley@gmail.com>
2023-08-14 14:57:12 +0100

    Extract update_legendary

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -13,6 +13,8 @@ class GildedRose
         update_brie(item)
       when "Backstage passes to a TAFKAL80ETC concert"
         update_pass(item)
+      when "Sulfuras, Hand of Ragnaros"
+        update_legendary(item)
       else
         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
           if item.quality > 0
@@ -76,24 +78,16 @@ class GildedRose
   end
 
   def update_pass(item)
-    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-      if item.quality > 0
-        if item.name != "Sulfuras, Hand of Ragnaros"
-          item.quality = item.quality - 1
+    if item.quality < 50
+      item.quality = item.quality + 1
+      if item.sell_in < 11
+        if item.quality < 50
+          item.quality = item.quality + 1
         end
       end
-    else
-      if item.quality < 50
-        item.quality = item.quality + 1
-        if item.sell_in < 11
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-        if item.sell_in < 6
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+      if item.sell_in < 6
+        if item.quality < 50
+          item.quality = item.quality + 1
         end
       end
     end
@@ -101,6 +95,9 @@ class GildedRose
     if item.sell_in < 0
       item.quality = 0
     end
+  end
+
+  def update_legendary(item)
   end
 end
 
