cf107e434e89462677c0c989791db76cf74c7da9
Kerry Buckley <kerryjbuckley@gmail.com>
2023-08-14 14:55:22 +0100

    Extract update_pass

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -11,6 +11,8 @@ class GildedRose
       case item.name
       when "Aged Brie"
         update_brie(item)
+      when "Backstage passes to a TAFKAL80ETC concert"
+        update_pass(item)
       else
         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
           if item.quality > 0
@@ -70,6 +72,34 @@ class GildedRose
       if item.quality < 50
         item.quality = item.quality + 1
       end
+    end
+  end
+
+  def update_pass(item)
+    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.quality > 0
+        if item.name != "Sulfuras, Hand of Ragnaros"
+          item.quality = item.quality - 1
+        end
+      end
+    else
+      if item.quality < 50
+        item.quality = item.quality + 1
+        if item.sell_in < 11
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+        if item.sell_in < 6
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
+    end
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0
+      item.quality = 0
     end
   end
 end
