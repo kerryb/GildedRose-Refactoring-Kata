09320d262d21b64f006870accccc4248f537bf5c
Kerry Buckley <kerryjbuckley@gmail.com>
2023-08-17 15:21:14 +0100

    Convert another expression to -=

--- ruby/lib/normal_decorator.rb
+++ ruby/lib/normal_decorator.rb
@@ -3,7 +3,7 @@ require_relative "gilded_rose"
 class NormalDecorator < ItemDecorator
   def update
     decrease_quality
-    @item.sell_in = @item.sell_in - 1
+    @item.sell_in -= 1
     if @item.sell_in < 0
       decrease_quality
     end
